31970e0d1f1f297b440c07f9cd5ad196cb9e1988
horalekjos <86661098+horalekjos@users.noreply.github.com>
2023-10-02 11:14:27 +0200

    Add files via upload

---
+++ 03_Promenna_string_zapis/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _03_Promenna_string_zapis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ahoj, jsem virtuální papoušek Lóra, rád opakuji!");
+            Console.WriteLine("Napiš něco: ");
+
+            /*
+            string vstup;   // vytnoření proměnné datového typu string
+            vstup = Console.ReadLine(); // uložní zadaného textu do proměnné vstup
+            */
+           
+            string vstup = Console.ReadLine();
+            //vstup = vstup + " " + vstup + " " + vstup + "!";*/ // modifikuji proměnou samotnou - ztrácím její původní hodnotu
+            string vstup2 = vstup + " " + vstup + " " + vstup + "!"; // Nová proměná umožní zachovat hodnotu původní proměnné
+           
+            
+            //Console.WriteLine(Console.ReadLine()); // Výpis toho co jsem zadal n konzoli
+
+
+           Console.WriteLine(vstup2);
+            Console.ReadKey();
+        }
+    }
+}
