e7a06cc8d1a15f6d75a27ac3c62288c100e31d3e
horalekjos <86661098+horalekjos@users.noreply.github.com>
2023-10-02 10:31:59 +0200

    Add files via upload

--- 01_Ahoj_svete/Program.cs
+++ 01_Ahoj_svete/Program.cs
@@ -1,17 +1,21 @@
-﻿using System;
-
-namespace _01_Ahoj_svete
+﻿namespace _01_Ahoj_svete
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.Write("Další text");
-            Console.Write(" ");
-            Console.WriteLine("Jedeme v jednom řádku");
-            Console.WriteLine("Odsazený text");
+           // Takto se vkládá komentář
+           Console.WriteLine("Ahoj toto je můj první program");
+            Console.ReadKey();
+           /* Tato část kódu nebude vykonávána
+            Console.Write("Ahoj ");
+            Console.Write("toto ");
+            Console.Write("je ");
+            Console.Write("múj ");
+            Console.Write("první ");
+           Console.Write("program ");*/
 
+            Console.ReadKey();
         }
     }
 }
