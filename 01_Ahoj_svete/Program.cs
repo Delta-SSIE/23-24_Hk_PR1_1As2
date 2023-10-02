3debb53e07795eeacea9bceb59c610eefad51a8b
horalekjos <86661098+horalekjos@users.noreply.github.com>
2023-10-02 10:28:37 +0200

    Add files via upload

---
+++ 01_Ahoj_svete/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _01_Ahoj_svete
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Write("Další text");
+            Console.Write(" ");
+            Console.WriteLine("Jedeme v jednom řádku");
+            Console.WriteLine("Odsazený text");
+
+        }
+    }
+}
