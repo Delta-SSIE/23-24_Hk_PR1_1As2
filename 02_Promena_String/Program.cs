5256aa7f87ac0e5ed8faf85d098caa5a1c8630e9
horalekjos <86661098+horalekjos@users.noreply.github.com>
2023-10-02 10:57:11 +0200

    Add files via upload

---
+++ 02_Promena_String/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _02_Promena_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Napiš jak se jmenuješ:");
+            string name;    //oznámím počítači, že budu používat proměnnou name
+                            // a bude to řetězec (kus textu) - string
+            name = Console.ReadLine();
+            Console.WriteLine("Ahoj " + name + "!");
+            string  name2 = "Ahoj " + name + "!"; // deklarovat proměnou při jejím prvním použití
+            Console.WriteLine(name2);
+            Console.WriteLine("Obsah proměnné name: "+name);
+            //Console.WriteLine(nameof(name)); // vypíše jak se daná proměnná jmenuje
+            Console.WriteLine("Obsah proměnné "+nameof(name)+" je "+name);
+            Console.ReadKey();
+        }
+    }
+}
