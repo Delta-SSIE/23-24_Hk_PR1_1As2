d1db0bb6f8a5ae4959e7cd431c9ebe270760a3d0
horalekjos <86661098+horalekjos@users.noreply.github.com>
2023-10-05 11:04:49 +0200

    Add files via upload

---
+++ 04_String_retezce/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _04_String_retezec
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string jmeno = "František";
+            int pocet = 4;
+
+            //Jak můžeme řetezec vypsat
+
+            Console.WriteLine(jmeno + " má " + pocet + " psy");
+            Console.WriteLine("{0} má {1} psy", jmeno, pocet);
+            Console.WriteLine($"{jmeno} má {pocet} psy");
+
+        }
+    }
+}
