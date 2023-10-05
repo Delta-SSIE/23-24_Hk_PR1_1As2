c934b5c136efe881dabd3a70fb6dc06a0c8b7656
horalekjos <86661098+horalekjos@users.noreply.github.com>
2023-10-05 12:06:46 +0200

    Add files via upload

---
+++ 07_Datovy_typ_Char/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _07_Datovy_typ_Char
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char pismeno = 'A';
+            Console.WriteLine("Písmenko je: " + pismeno);
+
+            char pismeno2 = (char)(pismeno + 1);
+            Console.WriteLine("Písmenko je: " + pismeno2);
+
+            char pismeno3 = (char)(pismeno + 'b');
+            Console.WriteLine("Písmenko je: " + pismeno3);
+           
+        }
+    }
+}
