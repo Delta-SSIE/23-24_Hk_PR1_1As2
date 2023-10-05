6af6c87c9a02f37dc23cc7f6fa11e8c431056139
horalekjos <86661098+horalekjos@users.noreply.github.com>
2023-10-05 11:24:15 +0200

    Add files via upload

---
+++ 05_Ciselne_datove_typy/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _05_Ciselne_datove_typy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nacteno;
+            Console.WriteLine("Zadej celé číslo");
+            
+            /* Toto je moc dlouhé a ukacané
+             nacteno = Console.ReadLine();
+
+
+             int cislo; //delkaruji pormněou typi INTEGER - celá čísla
+             cislo = int.Parse(nacteno); // Přetypování datového typu string na integer
+             //cislo = cislo + 1; // zvyšuji hodnotu proměnné číslo o 1
+             cislo++; // inkrementace hodnoty o 1 místo cislo = cislo + 1
+             Console.WriteLine($"Zvětšené číslo má novou hodnotu {cislo}");
+             */
+
+            // Zlatá třední cesta
+            int cislo = int.Parse(Console.ReadLine());
+            cislo++;
+            Console.WriteLine($"Zvětšené číslo má novou hodnotu {cislo}");
+
+            // extremní minimalistická verze
+            //Console.WriteLine("Zvětšeno: " + (int.Parse(Console.ReadLine()) + 1));
+
+
+        }
+    }
+}
