6a0ed2d32b7227cf1d93a6f1059fc72d135d358b
horalekjos <86661098+horalekjos@users.noreply.github.com>
2023-10-09 10:37:12 +0200

    Add files via upload

---
+++ 09_obsah_obdelniku/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _09_obsah_obdelnik
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadej stanu a obdelníku");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Zadej stanu a obdelníku");
+            int b = int.Parse(Console.ReadLine());
+            int obsah = a * b;
+            int obvod = 2 * (a + b);
+            Console.WriteLine($"Obdelnik o staně {a} a {b} má obvod {obvod} a obsah {obsah}");
+            Console.ReadKey();
+        }
+    }
+}
