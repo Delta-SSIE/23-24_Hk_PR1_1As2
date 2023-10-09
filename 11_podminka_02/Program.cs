39d1b57035fd273e8fd68c562686526ee182c8bd
horalekjos <86661098+horalekjos@users.noreply.github.com>
2023-10-09 10:44:33 +0200

    Add files via upload

---
+++ 11_podminka_02/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _11_podminka_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Kolik je ti let: ");
+            string nacteno = Console.ReadLine();
+            int vek = int.Parse(nacteno);
+
+            if (vek < 18)
+            {
+                Console.WriteLine("Jdi raději do kina");
+            }
+            else
+            {
+                Console.WriteLine("OK můžeš kít volit");
+            }
+
+            if (vek == 18)
+            {
+                Console.WriteLine("…ale je to natěsno");
+            }
+        }
+    }
+}
