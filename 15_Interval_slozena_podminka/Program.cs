bc4fd56fa376602f024e194fd993c77e0ab2cee5
horalekjos <86661098+horalekjos@users.noreply.github.com>
2023-10-12 11:47:44 +0200

    Add files via upload

---
+++ 15_Interval_slozena_podminka/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _15_Interval_slozena_podminka
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadej číslo v intervalu <-10 ; -5 > nebo <30; 40>");
+            int a = int.Parse(Console.ReadLine());
+
+            if (((a >= -10) && (a <= -5)) || ((a >= 30) && (a <= 40)))
+
+                Console.WriteLine($"Tvé zadané číslo {a} leží v požadovaném intervalu");
+            else
+                Console.WriteLine($"Tvé zadané číslo {a} NEleží v požadovaném intervalu");
+
+
+            Console.ReadKey();
+        }
+    }
+}
