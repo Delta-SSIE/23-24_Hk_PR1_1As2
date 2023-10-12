4fa7396191b38fa8d943294d321cdec6755ff01c
horalekjos <86661098+horalekjos@users.noreply.github.com>
2023-10-12 10:50:08 +0200

    Add files via upload

---
+++ 13_Objev_povrch_složená pomínka/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Test_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadejte tři rozměry (délky stran) krychle nebo kvádru:");
+
+            // Načteme tři celá čísla ze vstupu
+            Console.Write("Strana a trojrozměrné tělesa: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Strana b trojrozměrné tělesa: ");
+            int b = int.Parse(Console.ReadLine());
+            Console.Write("Strana c trojrozměrné tělesa: ");
+            int c = int.Parse(Console.ReadLine());
+
+            if (a < 1 || b < 1 || c < 1)
+            {
+                Console.WriteLine("NEJEDNÁ se o krychli ani kvádr.");
+            }
+            else
+            if (a == b && b == c)
+            {
+                // Všechny strany jsou stejné, jedná se o krychli
+                int objem = a * a * a;
+                int povrch = 6 * a * a;
+                Console.WriteLine("Jedná se o krychli.");
+                Console.WriteLine($"Objem krychle: {objem}");
+                Console.WriteLine($"Povrch krychle: {povrch}");
+            }
+            else
+            {
+                int objem = a * b * c;
+                int povrch = 2 * (a * b + b * c + a * c);
+                Console.WriteLine("Jedná se o kvádr.");
+                Console.WriteLine($"Objem kvádru: {objem}");
+                Console.WriteLine($"Povrch kvádru: {povrch}");
+            }
+
+
+
+        }
+    }
+}
