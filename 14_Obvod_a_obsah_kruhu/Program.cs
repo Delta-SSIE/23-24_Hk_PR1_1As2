d7ecfc1c8f20935acd4322922db998bcce4abe8b
horalekjos <86661098+horalekjos@users.noreply.github.com>
2023-10-12 11:39:43 +0200

    Add files via upload

---
+++ 14_Obvod_a_obsah_kruhu/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _14_Obvod_a_obsah_kruhu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadej poloměr kružice");
+            double polomer = double.Parse(Console.ReadLine());
+            if (polomer < 0)
+                Console.WriteLine("Špatně zadaný poloměr");
+            else
+            {
+                double obvod = 2 * Math.PI * polomer;
+                double obsah = Math.PI * Math.Pow(polomer, 2); //Pow = druhá mocnina proměnné poloměr
+                Console.WriteLine($"Kužnice o poloměru {polomer:N3} má obvod {obvod:N3} a obsah {obsah:N3}");
+            }
+        }
+    }
+}
