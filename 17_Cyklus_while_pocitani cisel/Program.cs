956bb5f8df2a90527ede67776cf5e808e83aa822
horalekjos <86661098+horalekjos@users.noreply.github.com>
2023-10-19 11:04:28 +0200

    Add files via upload

---
+++ 17_Cyklus_while_pocitani cisel/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _17_Cyklus_while_počítání_čísel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadej konečné číslo");
+            int final = int.Parse(Console.ReadLine());
+            int soucet= 0;
+
+            /* while (soucet <= final)
+             {
+                 Console.WriteLine(soucet);
+                 soucet = soucet + 1;
+
+                 Console.ReadKey();
+             }*/
+            while (soucet < final)
+           {
+                soucet = soucet + 1;
+                Console.WriteLine(soucet);
+                Console.ReadKey();
+           }
+
+
+        }
+    }
+}
