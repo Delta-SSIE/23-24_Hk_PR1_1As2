60ffd0bf3aa7d47ed88b600f65777682cc2e4e82
horalekjos <86661098+horalekjos@users.noreply.github.com>
2023-10-19 11:20:09 +0200

    Add files via upload

---
+++ 20_Vnoreny_cyklus/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _20_Vnoreny_cyklus
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cislo;
+            Console.WriteLine("Zadej číslo");
+            while (!int.TryParse(Console.ReadLine(), out cislo))
+            {
+                Console.WriteLine("Má to být číslo.");
+            }
+            int soucet = cislo;
+            while (soucet < 1000)
+            {
+                Console.WriteLine("Zadej číslo");
+                while (!int.TryParse(Console.ReadLine(), out cislo))
+                {
+                    Console.WriteLine("Má to být číslo.");
+                }
+                soucet += cislo;
+            }
+
+
+            Console.WriteLine("Celkový součet je: {0}", soucet);
+        }
+    }
+}
