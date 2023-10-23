84e6a9d1ee948dc99fa348952a05fa13e36e0929
horalekjos <86661098+horalekjos@users.noreply.github.com>
2023-10-23 10:31:52 +0200

    Add files via upload

---
+++ 22_do_while_random/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Test02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random generator = new Random();
+            int cislo;
+            int i;
+            bool znova;
+            do
+            {
+                cislo = generator.Next(-5, 5);
+                i = 1;
+                while ((cislo % 2) == 0)
+                {
+                    cislo = generator.Next(-5, 6);
+                    i++;
+                }
+                Console.WriteLine($"\nLiché cislo {cislo} jsem vygeneroval na {i}. pokus.\n ");
+                Console.WriteLine($"Chceš generování opakovat? N/Y\n");
+                znova = (Console.ReadKey().KeyChar != 'Y') ? false:true; //  ternární operátor
+            } while (znova);
+        }
+    }
+}
