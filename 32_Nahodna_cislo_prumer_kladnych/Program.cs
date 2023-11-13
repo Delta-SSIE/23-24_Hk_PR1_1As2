2691315009b479eae43202f4463083db8150aa82
horalekjos <86661098+horalekjos@users.noreply.github.com>
2023-11-13 10:13:52 +0100

    Add files via upload

---
+++ 32_Nahodna_cislo_prumer_kladnych/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace _32_Nahodna_cislo_prumer_kladnych
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int pocetHodnot = 20;
+            int soucet = 0;
+            int pocetKladnychHodnot = 0;
+
+            int i = 0;
+
+            while (i < pocetHodnot)
+            {
+                int nahodnaHodnota = random.Next(-10, 11);
+
+                // Pokud je hodnota kladná, vypíšeme ji a přičteme k součtu
+                if (nahodnaHodnota > 0)
+                {
+                    Console.WriteLine("Kladná hodnota: " + nahodnaHodnota);
+                    pocetKladnychHodnot++;
+                    soucet += nahodnaHodnota;
+                }
+
+                i++;
+            }
+
+            // Pokud byly vygenerovány alespoň jedna kladná hodnota, vypočítáme průměr
+            if (pocetKladnychHodnot > 0)
+            {
+                double prumer = (double)soucet / pocetKladnychHodnot;
+                Console.WriteLine("Průměr kladných hodnot: " + prumer);
+            }
+            else
+            {
+                Console.WriteLine("Nebyly vygenerovány žádné kladné hodnoty.");
+            }
+
+            Console.ReadLine(); // Pauza na zobrazení výsledků
+        }
+    }
+}
